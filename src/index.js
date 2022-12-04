29a1a2f3875a7f87ddbb5764919e38ac7e8ab9bd
upretim <manojkumar.upreti@live.com>
2022-12-04 10:47:04 +0530

    added styles

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./router/app";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./styles.scss";
-
+//$env:PORT=5000
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
